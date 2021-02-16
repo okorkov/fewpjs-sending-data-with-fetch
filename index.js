b3d2192286315e00c6d880aa66f2997a6e72abf7
okorkov <alex.okorkov@gmail.com>
2021-02-16 12:30:38 -0800

    commit tree 6834900528a07d0e9215ec47b7959823a679b7af

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(usersName, usersEmail) {
+  const config = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({'name': usersName, 'email': usersEmail})
+  }
+  return fetch('http://localhost:3000/users', config)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+    let pi = document.createElement('p')
+    pi.innerHTML = object['id']
+    document.body.appendChild(pi)
+  })
+  .catch(function(error) {
+    alert("Unauthorized Access");
+    let pi = document.createElement('p')
+    pi.innerHTML = error.message;
+    document.body.appendChild(pi)
+  }); 
+}
